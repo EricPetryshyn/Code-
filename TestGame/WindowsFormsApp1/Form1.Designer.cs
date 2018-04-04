7158ac0f3ca0135e71c84e6866cc4a3aed5fa123
epetryshyn <epetryshyn17@gmail.com>
2018-04-03 22:24:43 -0700

    C# recent game work

---
+++ TestGame/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace TestGame
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+        
+            this.Timer1.Enabled = true;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+         
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 595);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Object Moving";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ TestGame/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGame
+{
+    public partial class Form1 : Form
+    {
+        private bool right;
+        private bool left;
+        private int x_value;
+        private int y_value;
+        private bool jump;
+        private int force;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            x_value = 50;
+            y_value = 500;
+            jump = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(new Bitmap("apple.png"), x_value, y_value, 32, 32);
+        }
+
+        private void Timer1_Tick(Object sender, EventArgs e)
+        {
+            if(right == true)
+            {
+                x_value += 10;
+            }
+            if(left == true)
+            {
+                x_value -= 10;
+            }
+
+            if (jump == true)
+            {
+                //falling for if the player has jumped before
+                y_value -= force;
+                force -= 5;
+            }
+            if (y_value > 530)
+            {
+                y_value = 530;
+                force = 0;
+                jump = false;
+            }
+            
+
+            Invalidate();
+        }
+
+        private void Form1_KeyDown(Object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    jump = true;
+                    force = 40;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(Object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+        }
+    }
+}
